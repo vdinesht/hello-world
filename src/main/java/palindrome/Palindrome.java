0d996507cd8f888694e112ea77153af6ed45ea3a
Dinesh Vincent <vincent_dinesh_t@cat.com>
2023-10-20 13:05:53 +0530

    Implemented command line app to accept string input and determine if its palindrome using Java Stack class.

--- src/main/java/palindrome/Palindrome.java
+++ src/main/java/palindrome/Palindrome.java
@@ -1,19 +1,5 @@
 package palindrome;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.List;
-import java.util.stream.Collectors;
-
-public class Palindrome {
-    public static void main(String[] args) {
-        System.out.println("Please enter a string: ");
-        List<String> lines = new BufferedReader(new InputStreamReader(System.in))
-                .lines().collect(Collectors.toList());
-        System.out.println("The value entered by you is: " + lines);
-
-
-    }
-
+public interface Palindrome {
+    boolean IsPalindrome(String word);
 }
